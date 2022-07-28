9a1d0d2e85a8a7f404a790f79d9228a570ae1592
Vladimir Shatskiy <vovaysach@gmail.com>
2022-07-29 02:50:35 +0300

    setup filtering in admin UI.

--- property/admin.py
+++ property/admin.py
@@ -4,6 +4,7 @@ from .models import Flat
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town',)
+    list_filter = ('new_building', 'rooms_number', 'has_balcony', )
     readonly_fields = ('created_at',)
     list_editable = ('new_building',)
     list_display = ('town', 'address','price', 'construction_year', 'new_building')
